6042e3bf8a541835e49bd795755d20d7b93055b5
Gerardo Barajas <123435420+GerardoB04@users.noreply.github.com>
2024-02-29 10:42:57 -0700

    moved scripts and updated life count

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Lives = 5;
+        Lives = 6;
         Cards = new List<Card>();
         Bet = 1;
     }
