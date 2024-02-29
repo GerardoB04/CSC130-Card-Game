574d771f19e3f92925477a088e59c7b3b4c363a1
Gerardo Barajas <123435420+GerardoB04@users.noreply.github.com>
2024-02-28 17:02:54 -0700

    simple start to player and card classes

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public byte Lives { get; private set; }
+    public List<Card> Cards { get; private set; }
+    public byte Bet { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Lives = 5;
+        Cards = new List<Card>();
+        Bet = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
